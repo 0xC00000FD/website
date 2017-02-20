64626dc4a6434b21b5954c50e9895f148b5de8a7
Saborknight <anton@brink.es>
2017-02-20 19:45:28 +0000

    Feature - Added in Google Calendar iFrame

--- calendar.php
+++ calendar.php
@@ -18,10 +18,7 @@ $pageTitle = 'calendar';
 					<div class="row">
 						<div class="12u">
 							<section id="content" style="text-align: center;">
-								<header>
-									<h2>Coming Soon!</h2>
-									<span>Our Hamsters are spinning the wheels of magic as fast as they can...</span>
-								</header>
+								<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=0esifq9bgklgt3cqipc7spgqgc%40group.calendar.google.com&amp;color=%23875509&amp;ctz=Europe%2FBerlin" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 							</section>
 						</div>
 					</div>
