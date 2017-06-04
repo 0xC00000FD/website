3a8488d7f2d9fdd691e04267737a5472fd76b48c
Saborknight <support@saborknight.com>
2017-06-04 20:28:19 +0100

    Maintenance - Modified Main homepage content + footer links

--- index.php
+++ index.php
@@ -31,33 +31,58 @@ $pageTitle = 'home';
 								<header>
 									<h2>Motto</h2>
 								</header>
-								<p><strong>160th Joint Special Forces: 'Army Rangers' is a tactical realism battlegroup. The 160th JSF is an elite airborne battle group designated Army Rangers assisted by support groups. It is mostly deployed in destabilized regions to restore peace and order.</strong></p>
+								<p><strong>The Joint Special Forces Airborne Regiment is a tactical realism battlegroup. Part of the Regiment is an elite infantry group 'AIG' assisted by Support and 'SFG'. It is mostly deployed in destabilized regions to restore peace and order.</strong></p>
 								<header>
 									<h2>Biography</h2>
 								</header>
 								<p>
-								The main group is divided into the main assault group mechanized infantry and special forces (Reconnaissance). The mechanized infantry specialises in quick deployment through paradrop and full frontal assault with heavy armoured elements as extra support. The Special Forces are highly trained in any kind of situation e.g hostage rescue and HVT elimination. The 'Army Rangers' are supported by rotary and fixed wing pilots and armoured vehicle crew. These groups provide logistic support and fire support.</p>
+								The main group consists of an elite airborne infantry group 'AIG' and Special Forces Group 'SFG' reconnaissance elements. The airborne infantry specialises in quick deployment through paradrop and full frontal assault with heavy armoured elements as extra support. The Special Forces are highly trained in any kind of situation e.g hostage rescue and HVT elimination. 'AIG' is supported by rotary and fixed wing pilots and armoured vehicle crew. These groups provide logistic support and fire support.</p>
 								<h3>Unit Philosophy</h3>
 								<ul class="style1">
-									<li>NATO ranking system adjusted to the player's team (Air Force - Army). Promotions are earned by participation in Special Operations (SO).</li>
-									<li>Higher ranked players cannot force a lower ranked player to do their bidding. Ranks are only in place for SOs.</li>
-									<li>Any inner unit disagreements are discussed publicly if possible, and taken to Command if agreements can not be reached.</li>
-									<li>Players can indicate their prefered role during the application. During the interview the final role/career path will be decided. It is entirely possible to change career path at any time given the proper arguments.</li>
-									<li>Players qualified in more than one field can indicate the pool they want to operate in before the briefing of an SO.</li>
-									<li>Everyone is free to speak their mind and present constructed critism.</li>
-									<li>Fun is one of the main goals and will always have a higher priority than performance.</li>
+									<li>Casual MILSIM (ACE3 and TFAR included).</li>
+									<li>EU based unit (UTC+0 - UTC+2)</li>
+									<li>Unofficial Operations (Monday - Friday, 20:00 UTC+2)</li>
+									<li>Official Operations (Friday - Sunday)</li>
+									<li>Soft age limit: 16+, 14+ allowed if mature (qualified by Staff)</li>
+									<li>3 Private Servers</li>
+									<li>3 Public Servers</li>
+									<li>Host to VIKING PMC server</li>
 								</ul>
-								<h3>Rules/Requirements</h3>
+								<header>
+									<h2>Features</h2>
+								</header>
 								<ul class="style1">
-									<li>Must have a working microphone and preferably experience with TFAR.</li>
-									<li>English is the main language in the unit and during operations. Therefore a moderate level of the English language is required.</li>
-									<li>Age for joining players is 16 or above. Players younger than the threshold can apply and are expected to act as maturely as the older players.</li>
-									<li>All members wear the same camouflage as their squad mates.</li>
-									<li>All members follow the chain of command during an operation.</li>
-									<li>All members have their kits sorted before the start of the SO to prevent waiting time.</li>
-									<li>All members have their mod pack updated and working before the start of an SO. If such is not the case, the player will not be allowed to participate.</li>
-									<li>All members attend 2 out of 4 SOs. Any player with more than 2 AWOLs will be removed due to inactivity. Sending a message to Staff about absence on time will prevent the AWOL count to increase.</li>
+									<li>All private servers support JSFAR addon-pack.</li>
+									<li>Basic to Advanced Training and Dedicated Role training.</li>
+									<li>Weekly Operations, private servers online 24/7 for private use.</li>
+									<li>Friendly and helpful community, joint operations with other communities.</li>
+									<li>A place to chill after a hard day, or play CO-OP horror scenarios at night!</li>
+									<li>A balance between performance and fun on the battlefield!</li>
 								</ul>
+								<strong>Global IP: 77.161.37.153</strong>
+								<ul class="style1">
+									<li>On Alpha server [2302] ACE3 Zeus SOCOM. Mods: JSFAR SW collection.</li>
+									<li>On Bravo server [2312] ACE3 Liberation Altis. Mods: JSFAR SW collection.</li>
+									<li>On Charlie server [2322] Invade and Annex. Mods: None.</li>
+									<li>On Delta server [2332] Escape from Tanoa. Mods: None.</li>
+									<li>On Echo server [2342] Stratis Insurgency (BMR). Mods: Mods allowed.</li>
+									<li>On Foxtrot server [2352] Exile 1.0.3. Mods: Exile 1.0.3, RyanZombies.</li>
+								</ul>
+								<h3>JSFAR Mods</h3>
+								<ul class="style1">
+									<li>Main Files (all required):<br /><a href="http://steamcommunity.com/sharedfiles/filedetails/?id=845910510">Main Files Steam Workshop Collection</li>
+									<li>Optional Files (Recommended for enhanced immersion: <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=935545021">Option Files Steam Workshop Collection</a></li>
+									<li>Server Only (Only required for the server or local hosted): <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=935626462">Server Only Steam Workshop Collection</a></li>
+								</ul>
+								<header>
+									<h2>Social</h2>
+								</header>
+								<h3>Unit Tag</h3>
+								<p>Do you want to join the JSFAR and carry our tag and emblem on the battlefield? Register here: <a href="https://units.arma3.com/unit/jsfar">JSFAR Units Page</a>!</p>
+								<h3>Public Steam Group</h3>
+								<p>Feel free to join our Public steam group! <a href="http://steamcommunity.com/groups/JSFAR">Public Steam Group</a></p>
+								<h3>Facebook Page</h3>
+								<p>Like us on facebook to show your support for the JSFAR! <a href="https://www.facebook.com/JSFARA3/">JSFAR Facebook Page</a></p>
 							</section>
 						</div>
 					</div>
