e4fa6fbe8d10ccab0216a00dc1e0c47b16dcbfce
GhostTheSlayer <GhostTheSlayer@users.noreply.github.com>
2018-04-27 16:04:31 +0200

    Index update and shit
    -fixed teamspeak widget so it works
    -updated the footer links, replaced FB with reddit
    -Updated all the info with the updated shit
    
    Bugs*☭:
    -FB icon needs to be replaced with reddit one ☭

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $pageTitle = 'home';
 <body class="homepage">
 	<?php include('inc/analytics.php'); ?>
 	<div class="contact">
-		<a href="contact.php"><p><span class="no-mobile">Contact JSFAR Command</span></p></a>
+		<a href="contact.php"><p><span class="no-mobile">Contact 7AA Command</span></p></a>
 	</div>
 	<div id="wrapper">
 		<?php include('layout/header.php'); ?>
@@ -22,7 +22,7 @@ $pageTitle = 'home';
 								<h2>Teamspeak</h2>
 							</header>
 							<?php include('inc/teamspeak.php'); ?>
-							<a href="ts3server://ts.jsfar.com" class="button">Join us on Teamspeak</a>
+							<a href="ts3server://ts.7thaa.com" class="button">Join us on Teamspeak</a>
 						</section>
 					</div>
 
@@ -31,17 +31,27 @@ $pageTitle = 'home';
 							<header>
 								<h2>Intro</h2>
 							</header>
-							<p><strong>The Joint Special Forces Airborne Regiment (founded in January 2016) is a tactical realism battlegroup. 2 active Platoons, 1 Air Wing and a Reserve Group make up the JSFA Regiment. The JSFAR is mostly deployed in destabilized regions to restore peace and order.</strong></p>
+							<p><strong>The 7th Air Assault Brigade (7AA) is multi-national ArmA 3 Tactical Realism group; a gaming community built around Bohemia Interactive's ArmA 3. 
+							The 7AA is based on the British 16th AAB, the Army's rapid response airborne formation, operating in the ERA of 2035. 
+							The 7th Air Assault Brigade is commonly deployed in politically sensitive and/or destabilised regions for classified reconnaissance operations. 
+							</strong></p>
 							<p>Check out our <a href="http://bit.ly/jsfar-orbat">ORBAT</a> to have a peak at the current structure of the unit.</p>
 							<header>
 								<h2>Biography</h2>
 							</header>
-							<p>The bulk of the JSFAR consists of Ravager Platoon. Ravager is a marine infantry Platoon and specialises in full frontal assault with heavy armoured elements in support. Ravager Platoon is also highly trained and capable of handling any kind of situation e.g hostage rescue and HVT elimination. Ravager is supported by rotary and fixed wing pilots from Valkyrie Wing and armoured vehicles from Rhino Platoon. Valkyrie and Rhino combine to provide durable and effective logistical and fire support.</p>
+							<p>
+							The brigade operates as light armoured infantry supported by light combat air assets and light armoured vehicles for a rapid deployment. 
+							The infantry of the brigade have all been trained up to SPEC OPS standards. Therefore, the 7AA's main MO are highly sensitive objectives, received from the Joint NATO Command Centre in Europe. 
+							This SPEC OPS infantry receives support from the Fire Support Group, the Royal Army Medical Corps and the Signals Squadrons. 
+							Together they make a lethal combination on the ground. During a full out war or a classified operation behind enemy lines, the 7AA can handle it all. 
+							As a rapid respone airborne formation, it is one of the few brigades capable of delivering Airborne operations.</p>
 							<h3>Unit Philosophy</h3>
 							<ul class="style1">
-								<li>Casual MILSIM (ACE3 and TFAR included).</li>
-								<li>EU based unit (UTC+0 - UTC+2)</li>
-								<li>Official Operations (Sundays)</li>
+								<li>The brigade falls under Special Forces with a UK Army-based rank structure.</li>
+								<li>Players are riflemen at the core. Specialisations are at heart's content.</li>
+								<li>Players qualified in more than one field can indicate the pool they want to operate in before the briefing of a mission. </li>
+								<li>Everyone is free to speak their mind and offer constructive criticism.</li>
+								<li>Fun is one of the main goals and will always have a higher priority than the seeking of control or false respect.</li>
 								<li>Soft age limit: 18+, 16+ allowed if mature (provided you pass the probation period)</li>
 								<li>2 Private Servers</li>
 								<li>5 Public Servers</li>
@@ -50,38 +60,38 @@ $pageTitle = 'home';
 								<h2>Features</h2>
 							</header>
 							<ul class="style1">
-								<li>All private servers support <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=845910510">JSFAR Modpack</a>.</li>
+								<li>All private servers support <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=845910510">7AA Modpack</a>.</li>
 								<li>Basic to Advanced Training and Dedicated Role training.</li>
-								<li>Weekly Operations, private servers online 24/7 for member use.</li>
+								<li>2 Weekly Operations, private servers online 24/7 for member use.</li>
 								<li>Friendly and helpful community, joint operations with other communities.</li>
 								<li>A place to chill after a hard day, or play CO-OP horror scenarios at night!</li>
 								<li>A balance between performance and fun on the battlefield!</li>
 							</ul>
-							<p><strong>Global Address: arma3.jsfar.com</strong></p>
+							<p><strong>Global Address: arma3.7thaa.com</strong></p>
 							<ul class="style1">
-								<li>On Alpha server [2302] TheNighstalker's Requiem For A Dream. Mods: JSFAR collection.</li>
-								<li>On Bravo server [2402] ACE3 Liberation Altis. Mods: JSFAR collection.</li>
-								<li>On Charlie server [2502] Malden 2035 Dynamic Universal War System. Mods: None.</li>
-								<li>On Delta server [2602] Escape from Tanoa. Mods: None.</li>
-								<li>On Echo server [2702] Stratis Insurgency (BMR). Mods: Mods allowed.</li>
-								<li>On Foxtrot server [2802] Escape from Malden: No mods.</li>
-								<li>On Foxtrot server [2902] Malden Combat Patrol: No mods.</li>
+								<li>On Alpha server [2302] Private Tactical Operations.</li>
+								<li>On Bravo server [2402] Private AliVE Tactical Operations.</li>
+								<li>On Charlie server [2502] Escape From Tanoa Mods: None.</li>
+								<li>On Delta server [2602] Escape from Altis. Mods: None.</li>
+								<li>On Echo server [2702] Zeus Game Master Altis Mods: None.</li>
+								<li>On Foxtrot server [2802] Zeus Game Master Tanoa Mods: None.</li>
+								<li>On Foxtrot server [2902] Zeus Game Master Malden Mods: None.</li>
 							</ul>
-							<h3>JSFAR Mods</h3>
+							<h3>7AA Mods</h3>
 							<ul class="style1">
 								<li>Main Files (all required): <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=845910510">Main Files Steam Workshop Collection</a></li>
-								<li>Optional Files (Recommended for enhanced immersion: <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=935545021">Option Files Steam Workshop Collection</a></li>
-								<li>Server Only (Only required for the server or local hosted): <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=935626462">Server Only Steam Workshop Collection</a></li>
+								<li>Optional Files (Recommended for enhanced immersion: <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=1237445004">Option Files Steam Workshop Collection</a></li>
+								<li>Terrain addons: <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=935545021">Server Only Steam Workshop Collection</a></li>
 							</ul>
 							<header>
 								<h2>Social</h2>
 							</header>
 							<h3>Unit Tag</h3>
-							<p>Do you want to join the JSFAR and carry our tag and emblem on the battlefield? Register here: <a href="https://bit.ly/jsfar-arma-3-units">JSFAR Units Page</a>!</p>
+							<p>Do you want to join the 7AA and carry our tag and emblem on the battlefield? Register here: <a href="https://units.arma3.com/unit/7AA">7AA Units Page</a>!</p>
 							<h3>Public Steam Group</h3>
-							<p>Feel free to join our Public steam group! <a href="http://steamcommunity.com/groups/JSFAR">Public Steam Group</a></p>
+							<p>Feel free to join our Public steam group! <a href="https://steamcommunity.com/groups/7thaa">Public Steam Group</a></p>
 							<h3>Facebook Page</h3>
-							<p>Like us on facebook to show your support for the JSFAR! <a href="https://www.facebook.com/JSFARA3/">JSFAR Facebook Page</a></p>
+							<p>Follow our reddit page!  <a href="https://www.reddit.com/r/7thAA/">7AA reddit Page</a></p>
 						</section>
 					</div>
 				</div>
--- layout/footer.php
+++ layout/footer.php
@@ -9,7 +9,7 @@
 								</header>
 								<ul class="style3">
 									<li class="first">
-										<p class="date"><a class="fullwidth" href="http://steamcommunity.com/groups/JSFAR">JSFAR Steam Group</a></p>
+										<p class="date"><a class="fullwidth" href="https://steamcommunity.com/groups/7thaa">7AA Steam Group</a></p>
 									</li>
 									<li>
 										<p>Find our Steam Group!</p>
@@ -24,7 +24,7 @@
 								</header>
 								<ul class="style3">
 									<li class="first">
-										<p class="date"><a class="fullwidth" href="https://bit.ly/jsfar-arma-3-units">JSFAR Arma 3 Units Page</a></p>
+										<p class="date"><a class="fullwidth" href="https://units.arma3.com/unit/7AA">7AA Arma 3 Units Page</a></p>
 									</li>
 									<li>
 										<p>We can also be found on the Arma 3 Units website!</p>
@@ -35,14 +35,14 @@
 						<div class="3rd">
 							<section id="box3">
 								<header class="section3">
-									<h2>Facebook</h2>
+									<h2>Reddit</h2>
 								</header>
 								<ul class="style3">
 									<li class="first">
-										<p class="date"><a class="fullwidth" href="https://www.facebook.com/JSFARA3/">JSFAR Facebook Page</a></p>
+										<p class="date"><a class="fullwidth" href="https://www.reddit.com/r/7thAA/">7AA Reddit Page</a></p>
 									</li>
 									<li>
-										<p>Visit our Facebook page for announcements!</p>
+										<p>Visit our Reddit page for announcements!</p>
 									</li>
 								</ul>
 							</section>
@@ -54,7 +54,7 @@
 			<!-- Copyright -->
 			<div id="copyright">
 				<div class="container">
-					<p style="margin-bottom: 0;">Copyright JSFAR &copy; 2017</p>
+					<p style="margin-bottom: 0;">Copyright 7AA &copy; 2018</p>
 					<p style="margin-bottom: 0.5em;">Implemented by <a href="http://www.saborknight.com">Saborknight Media</a></p>
 				</div>
 			</div>
